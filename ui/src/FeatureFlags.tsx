a4b68a7dcb16f49f9658db4d2c0ac35bbb5f3de1
Daniel Aschwanden <daniel.aschwanden@f-eld.ch>
2024-10-28 15:04:09 +0100

    feat(feature-flags): make feature flag evaluation context easier

--- ui/src/FeatureFlags.tsx
+++ ui/src/FeatureFlags.tsx
@@ -1,7 +1,8 @@
 import { EvaluationContext, OpenFeatureProvider, OpenFeature, InMemoryProvider } from "@openfeature/react-sdk";
 import { FliptWebProvider } from "@openfeature/flipt-web-provider";
+import { PropsWithChildren, useEffect, useContext } from "react";
 
-import { PropsWithChildren, useEffect } from "react";
+import { UserContext } from "utils";
 
 const localFlagConfig = {
   "new-message": {
@@ -22,13 +23,24 @@ const localFlagConfig = {
 
 const Provider = (props: PropsWithChildren) => {
   const { children } = props;
+  const userState = useContext(UserContext);
+
   useEffect(() => {
     const fliptProvider = new FliptWebProvider("sitrep-ui", { url: "https://flipt.sitrep.ch" });
     OpenFeature.setLogger(console);
     OpenFeature.setProvider("local", new InMemoryProvider(localFlagConfig));
-    console.log("setting origin", document.location.host);
-    OpenFeature.setProvider(fliptProvider, { domain: document.location.host.split(":")[0] });
+    OpenFeature.setProvider(fliptProvider);
   }, []);
+
+  // sync the evaulation context here, so far only depends on domain and UserContext state
+  useEffect(() => {
+    const context = {
+      domain: document.location.host.split(":")[0],
+      email: userState.email,
+    };
+    OpenFeature.setContext(context);
+    console.log("setting context", context);
+  }, [userState]);
 
   return <OpenFeatureProvider>{children}</OpenFeatureProvider>;
 };
