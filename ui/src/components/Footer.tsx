21a9a60c90a963b921b2ceb5655b982e89a7c594
lukastresch <101600611+lukastresch@users.noreply.github.com>
2024-11-07 21:06:10 +0100

    fix(ui): text color in footer changed to be adaptive (#500)
    Co-authored-by: Lukas Tresch <lukas.tresch@students.fhnw.ch>

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -8,26 +8,28 @@ function Footer() {
     <footer className="footer is-hidden-print">
       <div className="content has-text-centered is-flex is-align-content-space-around is-align-items-center is-justify-content-center">
         <a
-          className="has-text-grey-light"
+          className="has-text-current"
           href="https://github.com/f-eld-ch/sitrep"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="icon-text is-small has-text-black">
+          <span className="icon-text is-small is-size-7">
             <span className="icon is-align-content-center">
               <FontAwesomeIcon icon={faGithub} />
             </span>
             <span>
-              <p className="has-text-dark">SitRep</p>
+              <strong>
+                <p className="is-size-7 is-family-monospace has-text-current">SitRep</p>
+              </strong>
             </span>
           </span>
         </a>
 
-        <p className="is-size-7 is-family-monospace ml-1">
-          made with <FontAwesomeIcon icon={faHeart} color="red" /> in Switzerland by{" "}
-          <strong>
-            <a className="has-text-dark" href="https://github.com/f-eld-ch" target="_blank" rel="noopener noreferrer">
-              F-ELD
+        <p className="is-size-7 is-family-monospace ml-2">
+          made with <FontAwesomeIcon icon={faHeart} color="red" /> in Switzerland by
+          <strong className="has-text-current">
+            <a className="has-text-current" href="https://github.com/f-eld-ch" target="_blank" rel="noopener noreferrer">
+              {" "}F-ELD
             </a>
           </strong>
         </p>
