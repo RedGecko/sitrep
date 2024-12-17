204bbaea4b402306a5e5f52c9a30e9509adbbb9a
Dani Aschwanden <daniel.aschwanden@f-eld.ch>
2024-12-17 13:25:53 +0100

    fix(i18n): format times and dates according to locale (#572)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { Navigate, Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import "./App.scss";
@@ -16,6 +16,7 @@ import { List as RequestList } from "views/measures/requests";
 import { List as TaskList } from "views/measures/tasks";
 import { List as ResourcesList } from "views/resource";
 
+import { useTranslation } from "react-i18next";
 import { ApolloProvider } from "@apollo/client";
 import { Spinner } from "components";
 import { UserProvider } from "utils";
@@ -23,10 +24,39 @@ import MessageSheet from "views/journal/MessageSheet";
 import { Layout, LayoutMarginLess } from "views/Layout";
 import { default as client } from "client";
 import { Provider as FeatureFlagProvider } from "FeatureFlags";
-
+import "./i18n";
+import dayjs from "dayjs";
+import LocalizedFormat from "dayjs/plugin/localizedFormat";
+import de from "dayjs/locale/de";
+import fr from "dayjs/locale/fr";
+import it from "dayjs/locale/it";
+import en from "dayjs/locale/en";
 const Map = lazy(() => import("views/map"));
 
 function App() {
+  const { i18n } = useTranslation();
+  dayjs.extend(LocalizedFormat);
+
+  useEffect(() => {
+    i18n.changeLanguage();
+    const locale = (lang: string) => {
+      switch (lang) {
+        case "de":
+          return de;
+        case "en":
+          return en;
+        case "fr":
+          return fr;
+        case "it":
+          return it;
+        default:
+          return en;
+      }
+    };
+    const lang = locale(i18n.language);
+    dayjs.locale(lang.toString());
+  }, [i18n.language]);
+
   return (
     <UserProvider>
       <ApolloProvider client={client}>
